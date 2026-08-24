1bdf59307a277d2b50664fce3a585faa7e90cca6
anabea212443 <anafariasraposo@gmail.com>
2026-08-24 10:08:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
+import streamlit as st
 
+st.title("🚨 Detector de Falhas")
+
+temperatura = st.number_input(
+    "Digite a temperatura do sensor:",
+    min_value=0.0,
+    max_value=150.0,
+    value=25.0
+)
+
+if temperatura > 80:
+    st.error("🔴 FALHA DETECTADA!")
+else:
+    st.success("🟢 Sensor funcionando normalmente")
